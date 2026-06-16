8c2c59ce61a3e99ab6ea5c77f016acf897f96e65
ApoJukai <johnharleydeleon@gmail.com>
2026-06-16 08:53:16 +0200

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Relative asset paths work well for GitHub Pages project sites:
+// https://USERNAME.github.io/REPOSITORY-NAME/
 export default defineConfig({
   plugins: [react()],
-  base: '/701/',
+  base: './',
 })
