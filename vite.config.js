9a1f9c7d65963d02f95aff58538e3ce21162630e
ApoJukai <johnharleydeleon@gmail.com>
2026-06-16 09:11:51 +0200

    Remove comments about GitHub Pages asset paths

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Relative asset paths work well for GitHub Pages project sites:
-// https://ApoJukai.github.io/701/
 export default defineConfig({
   plugins: [react()],
   base: '/701/',
